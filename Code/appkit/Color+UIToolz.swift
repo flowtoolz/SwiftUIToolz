4ee4dbf4c533da0cf17ce131548162158121a26d
Sebastian <contact@flowtoolz.com>
2018-05-25 01:56:10 +0200

    add initializer, add cgColor property

--- Code/appkit/Color+UIToolz.swift
+++ Code/appkit/Color+UIToolz.swift
@@ -3,6 +3,16 @@ import SwiftyToolz
 
 public extension Color
 {
+    init(_ nsColor: NSColor)
+    {
+        self.init(Float(nsColor.redComponent),
+                  Float(nsColor.greenComponent),
+                  Float(nsColor.blueComponent),
+                  Float(nsColor.alphaComponent))
+    }
+    
+    var cgColor: CGColor { return nsColor.cgColor }
+    
     var nsColor: NSColor
     {
         return NSColor(calibratedRed: CGFloat(self.red),

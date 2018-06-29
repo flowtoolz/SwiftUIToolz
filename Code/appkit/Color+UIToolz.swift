0f34554cbae77980931b83f461f32ab59760ef94
Sebastian <contact@flowtoolz.com>
2018-06-29 09:14:56 +0200

    remove self.

--- Code/appkit/Color+UIToolz.swift
+++ Code/appkit/Color+UIToolz.swift
@@ -15,9 +15,9 @@ public extension Color
     
     var nsColor: NSColor
     {
-        return NSColor(calibratedRed: CGFloat(self.red),
-                       green: CGFloat(self.green),
-                       blue: CGFloat(self.blue),
-                       alpha: CGFloat(self.alpha))
+        return NSColor(calibratedRed: CGFloat(red),
+                       green: CGFloat(green),
+                       blue: CGFloat(blue),
+                       alpha: CGFloat(alpha))
     }
 }

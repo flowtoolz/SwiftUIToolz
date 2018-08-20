b9a7f71f454f82757a9cc84c11fa48e69728270b
Sebastian <contact@flowtoolz.com>
2018-08-20 08:12:19 +0200

    adjust default icon scaling for use of pdf images

--- Code/appkit/Icon.swift
+++ Code/appkit/Icon.swift
@@ -2,13 +2,17 @@ import AppKit.NSImageView
 
 public class Icon: NSImageView
 {
-    public init(with image: NSImage)
+    public init(with image: NSImage,
+                scaling: NSImageScaling = .scaleProportionallyUpOrDown)
     {
         super.init(frame: .zero)
         
         self.image = image
-        imageScaling = .scaleNone
+        imageScaling = scaling
         imageAlignment = .alignCenter
+        
+        setContentHuggingPriority(.required, for: .horizontal)
+        setContentHuggingPriority(.required, for: .vertical)
     }
     
     public required init?(coder: NSCoder) { fatalError() }

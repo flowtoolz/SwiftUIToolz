bc325f09fb98046eaf2fa29a583b4e8eb6bcd798
Sebastian <contact@flowtoolz.com>
2018-09-11 16:36:27 +0200

    add functions to easily create and add layout guides

--- Code/appkit/NSView+AutoLayout.swift
+++ Code/appkit/NSView+AutoLayout.swift
@@ -1,4 +1,5 @@
 import AppKit.NSView
+import AppKit.NSLayoutGuide
 
 public extension NSView
 {
@@ -9,5 +10,34 @@ public extension NSView
         addSubview(view)
         
         return view
+    }
+    
+    @available(OSX 10.11, *)
+    func addLayoutGuide() -> NSLayoutGuide
+    {
+        let guide = NSLayoutGuide()
+        
+        addLayoutGuide(guide)
+        
+        return guide
+    }
+    
+    @available(OSX 10.11, *)
+    func addLayoutGuides(_ number: Int) -> [NSLayoutGuide]
+    {
+        guard number > 0 else { return [] }
+        
+        var guides = [NSLayoutGuide]()
+        
+        for _ in 0 ..< number
+        {
+            let guide = NSLayoutGuide()
+            
+            addLayoutGuide(guide)
+            
+            guides.append(guide)
+        }
+        
+        return guides
     }
 }

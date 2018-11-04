28ebba74c99e95bd2fe35595a7f00ebb00d9ae80
Sebastian <contact@flowtoolz.com>
2018-11-04 16:24:59 +0100

    move to app controller: handling dark mode, window, log prefix

--- Code/appkit/AppController.swift
+++ Code/appkit/AppController.swift
@@ -1,13 +1,17 @@
 import AppKit
 import SwiftObserver
-//import FoundationToolz
+import FoundationToolz
 
-open class AppController: NSObject, NSApplicationDelegate, Observer
+open class AppController: NSObject, NSApplicationDelegate, NSWindowDelegate, Observer
 {
+    // MARK: - Initialization
+    
     public init(withMainMenu mainMenu: NSMenu? = nil)
     {
         super.init()
-    
+        
+        Log.prefix = (appName ?? "UIToolz App").uppercased()
+        
         _ = NSApplication.shared // initializes app
     
         NSApp.mainMenu = mainMenu // must be set before delegate
@@ -15,9 +19,13 @@ open class AppController: NSObject, NSApplicationDelegate, Observer
         NSApp.run()
     }
     
+    // MARK: - App Delegate
+    
     open func applicationDidFinishLaunching(_ aNotification: Notification)
     {
         NSApp.activate(ignoringOtherApps: true)
+        
+        observeSystemAppearance()
         
         observe(Log.shared.latestEntry)
         {
@@ -26,6 +34,63 @@ open class AppController: NSObject, NSApplicationDelegate, Observer
         
         //networkReachability.setup()
     }
+    
+    open func applicationWillBecomeActive(_ notification: Notification)
+    {
+        // TODO: why does the window lose the delegate anyway??
+        window.delegate = self
+        window.show()
+    }
+    
+    // MARK: - Window
+    
+    open func windowDidEndLiveResize(_ notification: Notification)
+    {
+        window.didEndLiveResize()
+    }
+    
+    open func windowDidEnterFullScreen(_ notification: Notification)
+    {
+        window.didEndLiveResize()
+    }
+    
+    open func windowDidExitFullScreen(_ notification: Notification)
+    {
+        window.didEndLiveResize()
+    }
+    
+    public let window = Window()
+    
+    // MARK: - Dark Mode
+    
+    private func observeSystemAppearance()
+    {
+        guard #available(OSX 10.14, *) else { return }
+        
+        appearanceObservation = NSApp.observe(\.effectiveAppearance)
+        {
+            [weak self] _, _ in self?.appAppearanceDidChange()
+        }
+    }
+    
+    @available(OSX 10.14, *)
+    private func appAppearanceDidChange()
+    {
+        systemDidChangeColorMode(dark: systemIsInDarkMode)
+    }
+    
+    private var appearanceObservation: NSKeyValueObservation?
+    
+    open func systemDidChangeColorMode(dark: Bool) {}
+    
+    public var systemIsInDarkMode: Bool
+    {
+        guard #available(OSX 10.14, *) else { return false }
+        
+        return NSApp.effectiveAppearance.name == .darkAqua
+    }
+    
+    // MARK: - Push Notifications
     
     public func registerForPushNotifications()
     {

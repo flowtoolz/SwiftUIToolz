96e3188ed95b2ff5349a1bc4a71af46794fd4fd1
Sebastian <contact@flowtoolz.com>
2018-11-05 20:52:41 +0100

    make scroll table accessible for subclassing

--- Code/appkit/ScrollTable.swift
+++ Code/appkit/ScrollTable.swift
@@ -1,15 +1,15 @@
 import AppKit
 
-public class ScrollTable<T: NSTableView>: NSScrollView
+open class ScrollTable<T: NSTableView>: NSScrollView
 {
-    override init(frame frameRect: NSRect)
+    public override init(frame frameRect: NSRect)
     {
         super.init(frame: frameRect)
         
         documentView = table
     }
     
-    required init?(coder: NSCoder) { fatalError() }
+    public required init?(coder: NSCoder) { fatalError() }
     
     public let table = T()
 }

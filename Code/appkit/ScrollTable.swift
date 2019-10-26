6a79f19337dfae72187a6d6c88e8523e8443535e
flowtoolz <contact@flowtoolz.com>
2019-10-26 20:17:18 +0200

    Try to disable horizontal scroll bouncing in ScrollTable

--- Code/appkit/ScrollTable.swift
+++ Code/appkit/ScrollTable.swift
@@ -5,8 +5,10 @@ open class ScrollTable<T: NSTableView>: NSScrollView
     public override init(frame frameRect: NSRect)
     {
         super.init(frame: frameRect)
-        
         documentView = table
+        hasHorizontalScroller = false
+        horizontalScroller = nil
+        horizontalScrollElasticity = .none
     }
     
     public required init?(coder: NSCoder) { fatalError() }

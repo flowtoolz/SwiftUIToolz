07dda4ee3f1f41628a5c88f6d605eb921b62d8c9
Sebastian Fichtner <contact@flowtoolz.com>
2021-10-28 16:20:45 +0200

    Bump minimum macOS platform version to 10.14 (Mojave)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let codePath = "Code/uikit"
 
 let package = Package(
     name: "UIToolz",
-    platforms: [.iOS(.v9), .tvOS(.v9), .macOS(.v10_12)],
+    platforms: [.iOS(.v9), .tvOS(.v9), .macOS(.v10_14)],
     products: [
         .library(
             name: "UIToolz",
